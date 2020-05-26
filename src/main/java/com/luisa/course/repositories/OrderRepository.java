5ec8a98ca6c4bee5d6254f3ea19b9276d613bedb
Luisa Azevedo <nunesazevedoluisa@gmail.com>
2020-05-25 22:11:34 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/luisa/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.luisa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luisa.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
