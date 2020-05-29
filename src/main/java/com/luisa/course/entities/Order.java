f3b1a3d7fc44db2e6bff61cb97d9198fa4845cca
Luisa Azevedo <nunesazevedoluisa@gmail.com>
2020-05-28 22:13:49 -0300

    Subtotal & Total methods

--- src/main/java/com/luisa/course/entities/Order.java
+++ src/main/java/com/luisa/course/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable{
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double sum =0;
+		for (OrderItem item : items) {
+			sum += item.getSubtotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
